c610978832aad87037ac54ad597ef61cd0fbafb0
To Be Human <67031062+Rif340@users.noreply.github.com>
2025-04-23 08:58:27 +0700

    Add files via upload

---
+++ latsol6_bag2.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+struct Queue
+{
+    Node *front;
+    Node *rear;
+};
+
+// Inisialisasi queue
+void initQueue(Queue *q)
+{
+    q->front = q->rear = NULL;
+}
+
+// Cek apakah queue kosong
+bool isEmpty(Queue *q)
+{
+    return (q->front == NULL && q->rear == NULL);
+}
+
+// Enqueue data
+void enqueue(Queue *q, int data)
+{
+    Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (isEmpty(q))
+    {
+        q->front = q->rear = newNode;
+    }
+    else
+    {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+}
+
+// Dequeue data
+void dequeue(Queue *q)
+{
+    if (isEmpty(q))
+    {
+        cout << "Queue kosong" << endl;
+    }
+    else
+    {
+        Node *temp = q->front;
+        q->front = q->front->next;
+        if (q->front == NULL)
+        {
+            q->rear = NULL;
+        }
+        cout << "Mengeluarkan data: " << temp->data << endl;
+        delete temp;
+    }
+}
+
+// Tampilkan isi queue
+void display(Queue *q)
+{
+    if (isEmpty(q))
+    {
+        cout << "Queue kosong" << endl;
+    }
+    else
+    {
+        Node *temp = q->front;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    Queue q;
+    initQueue(&q);
+
+    int pilih, data;
+    do
+    {
+        cout << "____________________" << endl;
+        cout << " Menu Pilihan" << endl;
+        cout << "--------------------" << endl;
+        cout << " [1] Enqueue" << endl;
+        cout << " [2] Dequeue" << endl;
+        cout << " [3] Tampilkan" << endl;
+        cout << " [4] Keluar" << endl;
+        cout << "____________________" << endl;
+        cout << "Masukkan pilihan: ";
+        cin >> pilih;
+
+        switch (pilih)
+        {
+        case 1:
+            cout << "Masukkan data: ";
+            cin >> data;
+            enqueue(&q, data);
+            break;
+        case 2:
+            dequeue(&q);
+            break;
+        case 3:
+            display(&q);
+            break;
+        case 4:
+            cout << "Keluar" << endl;
+            break;
+        default:
+            cout << "Pilihan tidak tersedia" << endl;
+        }
+    } while (pilih != 4);
+
+    return 0;
+}
